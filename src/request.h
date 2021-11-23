57c13bcefdebab1a06933e1150f2ac41ff7979c5
Andres Cespedes <andresrcespedes@gmail.com>
2021-11-23 17:36:36 +0100

    Changes in the wserver.h and request.h
    wserver.c: added the threads and buffer flags. Also created the threads pool.
    
    request.h: definition of the threads and buffer default value (1 for both and its taken from the ReadME file)

--- src/request.h
+++ src/request.h
@@ -1,4 +1,9 @@
 #ifndef __REQUEST_H__
+#define ThreadsDefault 1
+#define BuffersDefault 1
+
+int threads;
+int buffersmax;
 
 void request_handle(int fd);
 
--- src/wserver.c
+++ src/wserver.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "request.h"
 #include "io_helper.h"
+#include <pthread.h> //We include this library in order to implement varius threads.
 
 char default_root[] = ".";
 
@@ -19,8 +20,14 @@ int main(int argc, char *argv[]) {
     int c;
     char *root_dir = default_root;
     int port = 10000;
+
+//We select the default values that where given in the ReadMe file. Those values are defined on the
+//Request.h file
+
+	threads=ThreadsDefault;   //amount of threads
+	buffermax=BuffersDefault; //Maximum size of the buffer
     
-    while ((c = getopt(argc, argv, "d:p:")) != -1)
+    while ((c = getopt(argc, argv, "d:p:t:b:")) != -1)
 	switch (c) {
 	case 'd':
 	    root_dir = optarg;
@@ -28,13 +35,30 @@ int main(int argc, char *argv[]) {
 	case 'p':
 	    port = atoi(optarg);
 	    break;
+	case 't':
+	    threads = atoi(optarg);
+	    break;
+	case 'b':
+	    buffers = atoi(optarg);
+	    break;
 	default:
-	    fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
+	// We change by adding the threads and the buffers
+	    fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffers]\n");
 	    exit(1);
 	}
 
     // run out of this directory
     chdir_or_die(root_dir);
+
+	//The Threads part:
+	// 1st we have to create a thread pool depending on the number of threads that we want
+	pthread_t thread_pool[threads]; //pthread_t is used to identify a thread
+	for(int i=0; i<threads; i++)
+	//create a new thread, as we want the default attributes for simplicity, we can left 
+	// it as NULL
+    	pthread_create(&thread_pool[i], NULL, thread_request_serve_static, NULL); //
+	// we can initialize the buffer size in 0
+	buffer = 0;	
 
     // now, get to work
     int listen_fd = open_listen_fd_or_die(port);
