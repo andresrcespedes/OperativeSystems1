603f24f4b23faaadff1e7c426511471ed1c046e9
Yoannez <qichao.hu@foxmail.com>
2021-12-16 06:56:09 -0800

    fix the command problem

--- src/test_bash/test.sh
+++ src/test_bash/test.sh
@@ -4,14 +4,18 @@ gnome-terminal --window --title='server' -- bash -c \
 "echo 'Server start!';
 echo 'port=10000 threads=3 buffers=3';
 echo; 
-../wserver -d . -p 10000 -t 3 -b 3"
+cd ..;
+./wserver -d . -p 10000 -t 3 -b 3;
+exec bash"
 
 gnome-terminal --window --title='dynamic request 1' \
 --tab --title='dynamic request 2' --tab --title='dynamic request 3' -- bash -c \
-"../wclient localhost 10000 spin.cgi;
+"cd ..;
+./wclient localhost 10000 /spin.cgi?30;
 exec bash"
 
 gnome-terminal --window --title='static request 1' \
 --tab --title='static request 2' -- bash -c \
-"../wclient localhost 10000 hello_world.html
+"cd ..;
+./wclient localhost 10000 /hello_world.html;
 exec bash"
